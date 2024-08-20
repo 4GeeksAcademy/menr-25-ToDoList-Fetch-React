c252d69791ef9c32afc89b0f0dd7a41454ebfcfc
Miguel Navas <143790463+menr-25@users.noreply.github.com>
2024-08-20 21:09:32 +0000

    Todolist usando React y Fetch done as requested

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,44 +1,124 @@
-import React, { useState } from "react";
-
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState, useEffect } from "react";
 
 const Home = () => {
+    const [inputValue, setInputValue] = useState("");
+    const [todos, setTodos] = useState([]);
+	const [showAlert, setShowAlert] = useState(false);
 
-	const [inputValue, setInputValue] = useState("");
-	const [todos, setTodos] = useState([]);
+	const loadTodos = () => {
+        fetch('https://playground.4geeks.com/todo/users/miguel_navas')
+            .then(response => response.json())
+            .then(data => {
+                setTodos(Array.isArray(data) ? data : []);
+            })
+    };
 
-	return (
-		<div>
+    useEffect(() => {
+        loadTodos();
+    }, []);
 
-			<h1>todos</h1>
-			<div className="shadow">
-				<ul>
-					<li>
-						<input	type="text" 
-								onChange={(e) => setInputValue(e.target.value)} 
-								value={inputValue}
-								onKeyDown={(e) => {
-									if (e.key === "Enter") {
-										setTodos(todos.concat(inputValue));
-										setInputValue("");
-									}
-								}}
-								placeholder="What needs to be done?">
-						</input>
-					</li>
+    const addTodo = () => {
+		if (inputValue.trim() === "") {
+            setShowAlert(true);
+            return;
+        }
 
-					{todos.map((item, index) => (
-						<li className="inputSize">
-							{item} <i className="fa-solid fa-x" onClick={() => setTodos(todos.filter((currentItem, currentIndex) => index != currentIndex))}></i>
-						</li>
-					))} 
-					
-				</ul>
+        setShowAlert(false); 
 
-				<div className="footer">{todos.length} item left</div>
-			</div>
-		</div>
-	);
+        const newTodo = { label: inputValue, is_done: false };
+        fetch('https://playground.4geeks.com/todo/todos/miguel_navas', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(newTodo),
+        })
+            .then(response => response.json())
+            .then(data => {
+                setTodos([...todos, data]);
+                setInputValue("");
+            })
+    };
+
+    const deleteTodo = (id) => {
+        fetch(`https://playground.4geeks.com/todo/todos/${id}`, {
+            method: 'DELETE',
+            headers: {
+                'accept': 'application/json'
+            }
+        })
+            .then(response => {
+                if (response.ok) {
+                    setTodos(todos.filter(todo => todo.id !== id));
+                } else {
+                    console.error('Error deleting todo:', response);
+                }
+            })
+    };
+
+    const deleteAllTodos = () => {
+        const deletePromises = todos.map(todo =>
+            fetch(`https://playground.4geeks.com/todo/todos/${todo.id}`, {
+                method: 'DELETE',
+                headers: {
+                    'accept': 'application/json'
+                }
+            })
+        );
+
+        Promise.all(deletePromises)
+            .then(() => setTodos([]))
+            .catch(error => console.error('Error deleting all todos:', error));
+    };
+
+    return (
+        <div>
+            <h1>todos</h1>
+            <div className="shadow">
+
+			{showAlert && (
+                    <div className="alert alert-danger" role="alert">
+                        Sorry, this field cannot be empty.
+                    </div>
+                )}
+
+                <ul>
+                    <li>
+                        <input
+                            type="text"
+                            onChange={(e) => setInputValue(e.target.value)}
+                            value={inputValue}
+                            onKeyDown={(e) => {
+                                if (e.key === "Enter") {
+                                    addTodo();
+                                }
+                            }}
+                            placeholder="What needs to be done?"
+                        />
+                    </li>
+
+                    {todos.map((item, index) => (
+                        <li key={index} className="inputSize">
+                            {item.label}{" "}
+                            <i
+                                className="fa-solid fa-x"
+                                onClick={() => deleteTodo(item.id)}
+                            ></i>
+                        </li>
+                    ))}
+                </ul>
+
+                <div className="footer">
+                    {todos.length} item left
+                </div>
+
+				<div className="button d-flex justify-content-center">
+					<button className="btn btn-danger" onClick={deleteAllTodos}>Delete All</button>
+				</div>
+            </div>
+        </div>
+    );
 };
 
 export default Home;
+
